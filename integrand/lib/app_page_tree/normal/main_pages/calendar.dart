a123baac70df124ef7d6d8583edc4505c677f77a
DoomKitty87 <doomkitty887@gmail.com>
2024-07-19 01:13:05 -0700

    decided not to do this rn

--- integrand/lib/app_page_tree/normal/main_pages/calendar.dart
+++ integrand/lib/app_page_tree/normal/main_pages/calendar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:integrand/backend/studentvue_api.dart';
+import 'package:integrand/consts.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
@@ -10,10 +12,32 @@ class Calendar extends StatefulWidget {
 class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Calendar'),
-      ),
+    return Column(
+      children: [
+        Row(
+          children: [
+            Center(
+              child: MonthDisplay()
+            ),
+          ]
+        ),
+      ]
+    );
+  }
+}
+
+class MonthDisplay extends StatelessWidget {
+  const MonthDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime currentTime = DateTime.now();
+    String month = month;
+    String year = year;
+
+    return Text(
+      month + ' ' + year,
+      style: boldBodyStyle,
     );
   }
 }
